50597a49a686d3c771bd910ea6b538ed3fd0cc36
James Carson <jcarson@monmouth.com>
2015-03-22 01:12:46 -0400

    parse sip messages

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ var EventEmitter = require('events').EventEmitter;
 var destination_host, destination_port, socket;
 var source_host, source_port;
 
+
+function parse_sip_response (response) {
+  response_hash = {};
+  sip_header_found = false;
+
+  function parse_sip_line (line) {
+    line = line.substring(0,line.length-1);
+    var space = line.indexOf(' ');
+    var name = line.substring(0,space);
+    var info = line.substring(space+1);
+
+    if ( name.slice(-1) == ':' ) {
+      response_hash[name.substring(0,name.length-1)] = info;
+    } else if ( name.length > 0 ) {
+      response_hash['Header'] = line;
+    }
+  }
+
+  var remaining_string = response;
+  while ( remaining_string.length > 0 ) {
+    var newline = remaining_string.indexOf('\n');
+    var current_string = remaining_string.substring(0,newline);
+    remaining_string = remaining_string.substring(newline + 1);
+    parse_sip_line(current_string);
+  }
+  console.log(response_hash);
+  return response_hash;
+}
+
 module.exports = new EventEmitter();
 
 module.exports.create = function (host,port) {
@@ -15,12 +44,9 @@ module.exports.create = function (host,port) {
     source_port = socket.address().port;
     module.exports.emit('ready');
   });
-  socket.on('message', function (data,info) {
-    console.log(data.toString());
-  });
 };
 
-module.exports.options = function () {
+module.exports.ping_server = function () {
   var message = new Buffer('OPTIONS sip:' + destination_host + ' SIP/2.0\n' +
     'Via: SIP/2.0/UDP ' + source_host + ':' + source_port + ';branch=z9hG4bK26534f84\n' +
     'Max-Forwards: 70\n' +
@@ -33,5 +59,8 @@ module.exports.options = function () {
     'Accept: text/plain\n' + 
     'Content-Length: 0\n'
   );
+  socket.on('message', function (data,info) {
+    hash = parse_sip_response(data.toString());
+  });
   socket.send(message, 0, message.length, destination_port, destination_host, function (err) {});
 };
