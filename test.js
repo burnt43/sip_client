87427b261e9f55c440fe506eeb21f72c7d6a6cd2
James Carson <jcarson@monmouth.com>
2015-03-29 02:18:08 -0400

    initial commit

---
+++ test.js
@@ -0,0 +1,7 @@
+var SipClient = require('./index.js');
+var sip_client = new SipClient("200.255.100.116","5060","mrfoobar","slapboat");
+
+sip_client.connect();
+sip_client.on('connection_established', function () {
+  sip_client.register();
+});
