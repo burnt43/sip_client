fb37711a85dcb233abb86caddf259d852edf64b6
James Carson <jcarson@monmouth.com>
2015-04-18 19:23:24 -0400

    removed the call code. it will be in its own class soon.

--- index.js
+++ index.js
@@ -48,38 +48,6 @@ SipClient.prototype.register = function () {
 }
 
 SipClient.prototype.place_call = function ( phone_number ) {
-/*  
-  var self = this;
-  var callid = this.create_callid();
-
-  function create_invite_message (nonce, response) {
-    if ( !nonce ) { nonce = self.generate_nonce(); }
-    if ( !response ) { response = self.generate_response(); }
-
-    return SipClientHelper.replace_values( SipMessageTemplates.invite, {
-      'source_address'      : self.sip_socket.get_source_address(),
-      'source_port'         : self.sip_socket.get_source_port(),
-      'destination_address' : self.sip_socket.get_sip_server_address(),
-      'destination_port'    : self.sip_socket.get_sip_server_port(),
-      'sip_callid'          : callid,
-      'sip_user_name'       : self.username,
-      'sip_nonce'           : nonce,
-      'sip_response'        : response,
-      'dialed_number'       : phone_number
-    });
-  }
-
-  this.sip_socket.on('401', function (data) {
-    console.log(data);
-    var nonce = data['WWW-Authenticate']['nonce'];
-    var realm = data['WWW-Authenticate']['realm'];
-    //need to rework generate_response to work for more than just registers
-    var response = SipClientHelper.md5_sum( SipClientHelper.md5_sum(self.username + ':' + realm + ':' + self.password) + ':' + nonce + ':' + SipClientHelper.md5_sum('INVITE:sip:'+phone_number+'@'+self.sip_socket.get_sip_server_address()) );
-    self.sip_socket.write ( create_invite_message( nonce, response ) );
-  });
-
-  this.sip_socket.write( create_invite_message() );
-*/
 }
 
 SipClient.prototype.__proto__ = require('events').EventEmitter.prototype;
