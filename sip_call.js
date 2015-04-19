39d79511a407bdd5dad7151c80fcfee67b38b225
James Carson <jcarson@monmouth.com>
2015-04-18 22:33:50 -0400

    removed logging statements from SipCall. SipCall now uses SipMessage.

--- sip_call.js
+++ sip_call.js
@@ -1,26 +1,26 @@
 var SipTransaction      = require('./sip_transaction.js');
 var SipHelper           = require('./sip_helper.js');
 var SipMessageTemplates = require('./sip_message_templates.js');
+var SipMessage          = require('./sip_message.js');
 
 SipCall.prototype.execute = function () {
 
-  var self = this;
+  var self                = this;
+  var initial_request     = new SipMessage( SipMessageTemplates.invite, this.message_data() );
+  var challenge_response  = null;
 
   this.create_listener('401', function (data) {
-    console.log('\033[0;36m');
-    console.log(data);
-    console.log('\033[0;39m');
     self.nonce = data['WWW-Authenticate']['nonce'];
     self.realm = data['WWW-Authenticate']['realm'];
     //self.sip_socket.write( self.message() );
   });
 
-  this.sip_socket.write( this.request() );
+  this.sip_socket.write( initial_request.get_message_string() );
 
 }
   
-SipCall.prototype.request = function () {
-  return SipHelper.replace_values( SipMessageTemplates.invite, {
+SipCall.prototype.message_data = function () {
+  return {
     'source_address'      : this.sip_socket.get_source_address(),
     'source_port'         : this.sip_socket.get_source_port(),
     'destination_address' : this.sip_socket.get_sip_server_address(),
@@ -30,14 +30,11 @@ SipCall.prototype.request = function () {
     'sip_nonce'           : this.get_nonce(),
     'sip_response'        : this.get_response(),
     'dialed_number'       : this.phone_number
-  });
+  };
 }
 
-SipCall.prototype.challenge_response = function () {
-  return '';
-}
-
-SipCall.prototype.message_name = function () { return 'INVITE'; }
+SipCall.prototype.log_color     = function () { return 'GREEN'; }
+SipCall.prototype.message_name  = function () { return 'INVITE'; }
 
 function SipCall (sip_client,sip_socket,phone_number) { 
   this.initialize(sip_client,sip_socket);
